1469b76a6904ccbb08083cd7937346a88765b337
mochan42 <mochan@students.42wolfsburg.de>
2023-03-27 08:50:53 +0200

    correct points initialization and constructors

--- ex02/src/FragTrap.cpp
+++ ex02/src/FragTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   FragTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 23:24:13 by mochan            #+#    #+#             */
-/*   Updated: 2023/03/26 23:38:56 by mochan           ###   ########.fr       */
+/*   Updated: 2023/03/27 08:49:01 by moninechan       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,18 @@
 FragTrap::FragTrap()
 {
 	std::cout << BLU << "Default constructor called from FragTrap" << D << "\n";
-	_name = "unknown";
-	_hitPoints	= 100;
-	_energyPoints = 50;
-	_attackDamage = 20;
+	this->_hitPoints	= 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
 }
 
 FragTrap::FragTrap(std::string setName)
 {
 	std::cout << BLU << "Constructor with name parameter called from FragTrap" << D << "\n";
-	_name = setName;
-	_hitPoints	= 100;
-	_energyPoints = 50;
-	_attackDamage = 20;
+	this->_name = setName;
+	this->_hitPoints	= 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
 }
 
 FragTrap::FragTrap(const ClapTrap& src)
